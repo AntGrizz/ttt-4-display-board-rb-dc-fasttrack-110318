85b19b693be83d400dab5b982ca57f561fc282fd
Anthony Gregg <agregg22@gmail.com>
2018-11-04 03:56:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -15,3 +15,24 @@ display_board(board)
 
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 display_board(board)
+
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
+
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+display_board(board)
+
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+display_board(board)
+
+board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+display_board(board)
+
+board = ["X", "X", "O", " ", "O", " ", "O", "X", " "]
+display_board(board)
